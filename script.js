7b76a588cb6accb1a4fb07f240f6d8a3d2fae7e4
Harrison <harrisondsouza76@gmail.com>
2022-10-18 12:37:35 +0530

    Create function to play one round

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // computer choice
 function getComputerChoice(){
     let choice = ["rock", "paper", "scissors" ];
-    console.log(choice[Math.floor(Math.random()*(choice.length))]);
+    return choice[Math.floor(Math.random()*(choice.length))];
     
     
 }
@@ -10,6 +10,24 @@ getComputerChoice();
 
 // ask for player input
 let playerChoice = prompt("Choose Rock, Paper or Scissors");
-console.log(playerChoice.toLowerCase());
+
+//play a round of game
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        console.log("Its a draw!");
+    } else if((computerSelection == "paper" && playerSelection == "rock")||(computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "paper")){
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    } else{
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    }
+}
+
+let playerSelection = playerChoice.toLowerCase();
+
+let computerSelection = getComputerChoice();
+
+console.log(`Computer chose ${computerSelection}`);
+
+playRound(playerSelection, computerSelection);
 
 
