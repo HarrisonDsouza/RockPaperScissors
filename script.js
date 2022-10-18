b1b97ea0d662f3218024321c82647c2a6d976aca
Harrison <harrisondsouza76@gmail.com>
2022-10-18 13:32:56 +0530

    Create loop to play 5 rounds keeping track of points

--- script.js
+++ script.js
@@ -8,26 +8,42 @@ function getComputerChoice(){
 }
 getComputerChoice();
 
-// ask for player input
-let playerChoice = prompt("Choose Rock, Paper or Scissors");
 
 //play a round of game
+
+
+
+// play 5 rounds
+let compPoints = 0;
+let playerPoints = 0;
+
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
         console.log("Its a draw!");
+        console.log(`Your point(s) : ${playerPoints} and Computer point(s) : ${compPoints}`);
     } else if((computerSelection == "paper" && playerSelection == "rock")||(computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "paper")){
         console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        compPoints++;
+        console.log(`Your point(s) : ${playerPoints} and Computer point(s) : ${compPoints}`);
     } else{
         console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerPoints++;
+        console.log(`Your point(s) : ${playerPoints} and Computer point(s) : ${compPoints}`);
     }
 }
 
-let playerSelection = playerChoice.toLowerCase();
 
-let computerSelection = getComputerChoice();
+for (let i=1; i <= 5; i++ ){
+    console.log(`Round : ${i}`);
+    
+    // ask for player input
+    let playerChoice = prompt("Choose Rock, Paper or Scissors");
 
-console.log(`Computer chose ${computerSelection}`);
+    //play round
+    let playerSelection = playerChoice.toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(`Computer chose ${computerSelection}`);
 
-playRound(playerSelection, computerSelection);
+    playRound(playerSelection, computerSelection);
 
-
+}
