9af2747a038bacbf8ae0906bd3652c72ba1f4127
Harrison <harrisondsouza76@gmail.com>
2022-10-18 12:01:20 +0530

    create function that asks user input

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+
+// computer choice
 function getComputerChoice(){
-    let choice = ["Rock", "Paper", "Scissors" ];
+    let choice = ["rock", "paper", "scissors" ];
     console.log(choice[Math.floor(Math.random()*(choice.length))]);
     
     
 }
 getComputerChoice();
+
+// ask for player input
+let playerChoice = prompt("Choose Rock, Paper or Scissors");
+console.log(playerChoice.toLowerCase());
+
+
