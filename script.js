0c8cdc1beaa56b8d7f42ec1df0eb08b20478bce2
Harrison <harrisondsouza76@gmail.com>
2022-10-30 22:22:31 +0530

    Create alert box to diplay final result

--- script.js
+++ script.js
@@ -24,31 +24,44 @@ function optionName(opt){
 
 
 
+// Game keeps going on until a player scores 5 points
 
-
+let compPoints = 0;
+let playerPoints = 0;
 
 const compPointsCount  = document.querySelector('.game-section .computer-points h1');
 const playerPointsCount  = document.querySelector('.game-section .player-points h1');
+const computerChoice = document.createElement('h2');
 const roundInfo =  document.createElement('p');
-function playRound(playerSelection, computerSelection){
+const popup = document.querySelector('#popup');
 
+function playRound(playerSelection, computerSelection){
+    
+    document.querySelector('.game-section .round-info').appendChild(computerChoice);
     document.querySelector('.game-section .round-info').appendChild(roundInfo);
     if(playerSelection == computerSelection){
+        computerChoice.textContent = `Computer chose ${computerSelection}`;
         roundInfo.textContent = "Its a draw!";
     } else if((computerSelection == "paper" && playerSelection == "rock")||(computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "paper")){
         compPoints++;
         if(compPoints == 5 && playerPoints < 5){
-            alert("computer wins");
+            document.querySelector('#popup h2').textContent = 'You Lose!';
+            popup.classList.add('active');
+            document.querySelector('#main').classList.toggle('blur');
             return;
         }
+        computerChoice.textContent = `Computer chose ${computerSelection}`;
         roundInfo.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
         compPointsCount.textContent = compPoints;
     } else{
         playerPoints++;
         if(playerPoints == 5 && compPoints < 5){
-            alert("you win");
+            document.querySelector('#popup h2').textContent = 'You Win!';
+            popup.classList.add('active');
+            document.querySelector('#main').classList.toggle('blur');
             return;
         }
+        computerChoice.textContent = `Computer chose ${computerSelection}`;
         roundInfo.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
         playerPointsCount.textContent = playerPoints;
     }
@@ -58,15 +71,17 @@ function playRound(playerSelection, computerSelection){
 }
 
 
-let compPoints = 0;
-let playerPoints = 0;
 
 
-
+// Start game when user selects an option
 let options = document.querySelectorAll('.options > div');
 options.forEach(option => {
     option.addEventListener('click', () => playRound(optionName(option.className), getComputerChoice()));
 });
+
+// play game again on clicking a button in the result popup
+let tryAgain = document.querySelector('#popup button');
+tryAgain.addEventListener('click', ()=>location.reload());
 
 
 
